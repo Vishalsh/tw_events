ee174f5a4a4b96b9b70fd715f8c7eb7699280852
Vishal Sharma <sharvishi9118@gmail.com>
2014-12-07 22:00:23 +0530

    Chnaged Okta configuration to point out to newly added app. Could figure out with faoce_ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_action :protected!
-  force_ssl
+  #force_ssl
   protect_from_forgery with: :exception
 
   def current_user
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,14 +2,14 @@ require 'omniauth'
 require 'omniauth-saml'
 require 'yaml'
 
-#OKTA_CONFIGS = YAML.load(File.read(File.join(Rails.application.config.root,"config", "okta.yml")))
+OKTA_CONFIGS = YAML.load(File.read(File.join(Rails.application.config.root,"config", "okta_saml.yml")))
 
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :saml,
-    :issuer                             => "https://enigmatic-beach-7933.herokuapp.com/topics",
-    :idp_sso_target_url                 => ENV["idp_sso_target_url"],
+    :issuer                             => "http://localhost:3000",
+    :idp_sso_target_url                 => OKTA_CONFIGS['development']['idp_sso_target_url'],
     :idp_sso_target_url_runtime_params  => {:redirectUrl => :RelayState},
-    :idp_cert_fingerprint               => ENV["idt_cert_fingerprint"],
+    :idp_cert_fingerprint               => OKTA_CONFIGS['development']['idp_cert_fingerprint'],
     :name_identifier_format             => "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
 end
