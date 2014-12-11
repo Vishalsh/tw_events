85c5407a84b749a8bc2acd2576867546a81bc5a0
Vishal Sharma <sharvishi9118@gmail.com>
2014-12-11 14:31:57 +0530

    Added some random functionlaity

--- app/assets/javascripts/datePicker.js
+++ app/assets/javascripts/datePicker.js
@@ -1,5 +1,8 @@
-$(function () {
-  $('.date-picker').datepicker().on('changeDate', function (ev) {
+function applyDatePicker() {
+  $('.date-picker').datepicker().on('changeDate', function () {
     $(this).datepicker('hide');
   });
-});
+}
+
+$(document).on('ready', applyDatePicker);
+$(document).on('page:load', applyDatePicker);
