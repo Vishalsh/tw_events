22d1dde70b880ef5d006fe6fe9a092b6f9f99cc9
Vishal Sharma <sharvishi9118@gmail.com>
2014-12-15 23:32:44 +0530

    Added tests for admin_users_helper

--- app/helpers/admin_users_helper.rb
+++ app/helpers/admin_users_helper.rb
@@ -2,9 +2,7 @@ module AdminUsersHelper
 
   def has_own_events
     admin_user = AdminUser.find_by(name: session[:user_name])
-    if admin_user
-      !admin_user.events.blank?
-    end
+    return !admin_user.events.blank? if admin_user
   end
 
 end
---
+++ spec/helpers/admin_users_helper_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe AdminUsersHelper do
+
+  context '#has_own_events' do
+    context 'when the logged in user is not an admin user' do
+      it 'should return nil' do
+        session[:user_name] = 'some other user'
+        create(:valid_admin_user)
+        expect(has_own_events).to eq nil
+      end
+    end
+  end
+
+  context '#has_own_events' do
+    context 'when the logged in user is an admin user' do
+      before(:each) do
+        session[:user_name] = 'some user'
+        @admin_user = create(:valid_admin_user)
+      end
+
+      it 'should return true if admin user has events' do
+        event1 = create(:valid_event)
+        event2 = create(:valid_event)
+        events = [event1, event2]
+        @admin_user.events = events
+        expect(has_own_events).to eq true
+      end
+
+      it 'should return false if admin user does not have events' do
+        expect(has_own_events).to eq false
+      end
+    end
+  end
+
+end
