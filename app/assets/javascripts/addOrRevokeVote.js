74f8e0f551700fe2e70f63f064bf366aad9b230c
Vishal Sharma <sharvishi9118@gmail.com>
2015-01-27 23:35:58 +0530

    Fixed disabling voting after voting close date

--- app/assets/javascripts/addOrRevokeVote.js
+++ app/assets/javascripts/addOrRevokeVote.js
@@ -1,42 +1,45 @@
 var bindToVote = function () {
-        $(".vote").click(function (e) {
-            e.preventDefault();
-            var target = $(this);
-            var id = target.attr('data-id');
-            if (target.hasClass('vote-open')) {
-                addVoteForTopic(id, target)
-            }
-            else {
-                revokeVoteFromTopic(id, target)
-            }
-        })
+
+  if (!$(".vote").hasClass('vote-disabled')) {
+    $(".vote").click(function (e) {
+      e.preventDefault();
+      var target = $(this);
+      var id = target.attr('data-id');
+      if (target.hasClass('vote-open')) {
+        addVoteForTopic(id, target)
+      }
+      else {
+        revokeVoteFromTopic(id, target)
+      }
+    });
+  }
 };
 
 var addVoteForTopic = function (id, target) {
 
-    $.ajax({
-        url: '/topics/vote_for/' + id,
-        type: 'GET',
-        crossDomain: true,
-        dataType: 'html',
-        success: function () {
-            target.addClass('vote-cast').removeClass('vote-open');
-        }
-    })
+  $.ajax({
+    url: '/topics/vote_for/' + id,
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'html',
+    success: function () {
+      target.addClass('vote-cast').removeClass('vote-open');
+    }
+  })
 }
 
 var revokeVoteFromTopic = function (id, target) {
 
-    $.ajax({
-        url: '/topics/revoke_vote/' + id,
-        type: 'GET',
-        crossDomain: true,
-        dataType: 'html',
-        success: function () {
-            target.addClass('vote-open').removeClass('vote-cast');
-        }
-    })
+  $.ajax({
+    url: '/topics/revoke_vote/' + id,
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'html',
+    success: function () {
+      target.addClass('vote-open').removeClass('vote-cast');
+    }
+  })
 }
 
-$(document).ready(bindToVote)
-$(document).on('page:load', bindToVote)
+$(document).ready(bindToVote);
+$(document).on('page:load', bindToVote);
