41a7d7443c889a781762bee28bc5f70462f0f2bb
Vishal Sharma <sharvishi9118@gmail.com>
2016-02-12 01:22:36 +0800

    commiting okta_saml.yml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,8 +8,8 @@ OKTA_CONFIGS = YAML.load(File.read(File.join(Rails.application.config.root,"conf
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :saml,
     :issuer                             => "http://localhost:3000",
-    :idp_sso_target_url                 => OKTA_CONFIGS['development']['idp_sso_target_url'],
+    :idp_sso_target_url                 => OKTA_CONFIGS['idp_sso_target_url'],
     :idp_sso_target_url_runtime_params  => {:redirectUrl => :RelayState},
-    :idp_cert_fingerprint               => OKTA_CONFIGS['development']['idp_cert_fingerprint'],
+    :idp_cert_fingerprint               => OKTA_CONFIGS['idp_cert_fingerprint'],
     :name_identifier_format             => "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
 end
